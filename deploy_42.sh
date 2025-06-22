7e547451d24e74e30ad094f07a5812bbf64aad2e
Giovanni Storti <gs151234@fi365.ort.edu.uy>
2025-06-22 16:01:04 -0300

    se arregla iptables para que no corte la conexión

---
+++ deploy_42.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "[1/2] Ejecutando bootstrap con puerto default..."
+ansible-playbook playbooks/bootstrap.yml -i inventory/hosts.ini \
+  -u sysadmin --ask-pass --ask-become-pass
+
+echo "[2/2] Esperando 10s para reinicio SSH..."
+sleep 10
+
+echo "[2/2] Ejecutando playhard con ansible y puerto 61189..."
+ansible-playbook playbooks/playhard.yml -i inventory/hosts.ini \
+  -u ansible --private-key ~/.ssh/ansible -e ansible_port=61189
